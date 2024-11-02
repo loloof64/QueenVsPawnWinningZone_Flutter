8df335dc923d319850a6ac5974e74265b1483dd4
laurent bernabé <laurent.bernabe@gmail.com>
2024-11-02 16:18:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# queen_vs_pawn_winning_zone
+
+Test yourself in the chess Queen vs Pawn endgame winning zone.
---
+++ android/app/src/main/kotlin/com/loloof64/queen_vs_pawn_winning_zone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.loloof64.queen_vs_pawn_winning_zone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
